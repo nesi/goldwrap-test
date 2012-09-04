d1058853276692407dc1671886f1e313cbf04b07
Martin Feller <martinfeller73@gmail.com>
2012-09-04 16:37:05 +1200

    add script for jenkins

---
+++ jenkins/run_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+venv="virtualenv"
+lettuce_output="goldwrap-test.out"
+
+function on_exit {
+  # purpose of call to sed: remove colors from lettuce output
+  if [ -f "${lettuce_output}" ]; then
+    cat ${lettuce_output} | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
+    rm -f ${lettuce_output}
+  fi
+}
+
+trap "on_exit" INT TERM EXIT
+
+if [ ! -d ${venv} ]; then
+  echo "############################ Create virtualenv ############################"
+  rm -f ${venv}
+  virtualenv ${venv}
+  source ${venv}/bin/activate
+  pip install -r requirements.pip
+fi
+
+echo "############################ Run selenium/lettuce tests ############################"
+source ${venv}/bin/activate
+lettuce > ${lettuce_output}
